cfd5c83649a4d7f86702de84913f36dae492be63
SorerBOT <alonfiller@gmail.com>
2025-12-16 13:35:30 +0200

    (Feat) assertion func

--- tests/test_lib.c
+++ tests/test_lib.c
@@ -1,11 +1,29 @@
 #include "stdio.h"
+#include <stddef.h>
+
+typedef enum
+{
+    false,
+    true
+} boolean;
+
+void assert(boolean condition)
+{
+    static size_t assertion_counter = 1;
+    if (condition == true)
+    {
+        printf("Assertion %lu: PASSED", assertion_counter);
+    }
+    else
+    {
+        printf("Assertion %lu: FAILED", assertion_counter);
+    }
+    ++assertion_counter;
+}
 
 void test1()
 {
-    if (1 + 1 == 2)
-    {
-        printf("Test 1 passed.\n");
-    }
+    assert(1+1 == 2);
 }
 
 int main()
