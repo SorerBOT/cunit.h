5853d3da36b778b4d9d77bb849df41416f4b375b
SorerBOT <alonfiller@gmail.com>
2025-12-16 14:52:31 +0200

    (Feat) now printing expression if failed

--- tests/test_lib.c
+++ tests/test_lib.c
@@ -1,13 +1,15 @@
 #include "stdio.h"
 #include <stddef.h>
 
+#define ASSERT(condition) assert((condition), (#condition))
+
 typedef enum
 {
     false,
     true
 } boolean;
 
-void assert(boolean condition)
+void assert(boolean condition, char* condition_expression)
 {
     static size_t assertion_counter = 1;
     if (condition == true)
@@ -16,14 +18,14 @@ void assert(boolean condition)
     }
     else
     {
-        printf("Assertion %lu: FAILED", assertion_counter);
+        printf("Assertion %lu, with expression %s: FAILED", assertion_counter, condition_expression);
     }
     ++assertion_counter;
 }
 
 void test1()
 {
-    assert(1+1 == 2);
+    ASSERT(1+2 == 2);
 }
 
 int main()
