d8837748e6c64edad41a3a38f60479c35cee9f71
SorerBOT <alonfiller@gmail.com>
2025-12-30 22:28:56 +0200

    FEAT added the test from the readme

---
+++ tests/preview_suite_from_readme.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "../lib/cunit.h"
+
+CUNIT_SETUP_ONETIME()
+{
+    printf("Establish connection to DB....\n");
+}
+CUNIT_CLEANUP_ONETIME()
+{
+    printf("Terminate connection to DB....\n");
+}
+CUNIT_SETUP()
+{
+    printf("Create DB entries for test....\n");
+}
+CUNIT_CLEANUP()
+{
+    printf("Delete DB after test....\n");
+}
+CUNIT_TEST(str_eq)
+{
+    CUNIT_ASSERT_STR_EQ("Hello World", "Hello World");
+    CUNIT_EXPECT_STR_NEQ(NULL, "Bimba");
+    CUNIT_EXPECT_STR_NEQ(NULL, NULL);
+}
